dc7887a9f356d84173418052a4a5099d8e4f91b6
Vlada Laukkonen <vlada@Vladas-MacBook-Pro.local>
2017-03-16 22:19:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/hannelandia/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.hannelandia;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
